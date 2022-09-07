a9becea6fbb4031076aac974b6368000ac3bc5eb
Rinaldo Nikilson <nikilson30@gmail.com>
2022-09-07 13:41:05 +0530

    GPG exe directly invoked

--- SavePassword/Form1.cs
+++ SavePassword/Form1.cs
@@ -69,6 +69,10 @@ namespace SavePassword
             else if (!String.Equals(textBox2.Text, textBox3.Text)){
                 MessageBox.Show("Your Passwords doesn't match!!", "Invalid Password", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
+            else if (!File.Exists("C:\\Program Files (x86)\\GnuPG\\bin\\gpg.exe"))
+            {
+                MessageBox.Show("Please install gpg for windows in C:\\Program Files (x86)\\GnuPG\\bin\\gpg.exe", "GPG Not Found", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
             else
             {
                 finalString += textBox2.Text;
@@ -100,18 +104,18 @@ namespace SavePassword
                 else
                 {
                     File.WriteAllText(passwordFile, finalString);
-                    string command = "gpg -r " + keyId + " -e " + passwordFile;
-                    ///MessageBox.Show(command, "Password Saved", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    /// string command = "gpg -r " + keyId + " -e " + passwordFile;
                     Process process = new Process();
-                    process.StartInfo.FileName = "CMD.exe";
+                    process.StartInfo.FileName = "C:\\Program Files (x86)\\GnuPG\\bin\\gpg.exe";
                     process.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
                     process.StartInfo.RedirectStandardOutput = true;
                     process.StartInfo.RedirectStandardError = true;
                     process.StartInfo.UseShellExecute = false;
                     process.StartInfo.CreateNoWindow = true;
-                    process.StartInfo.Arguments = "/C " + command + " && del " + passwordFile;
+                    process.StartInfo.Arguments = "-r " + keyId + " --always-trust" + " -e " + passwordFile;
                     process.Start();
-                    ///File.Delete(passwordFile);
+                    process.WaitForExit();
+                    File.Delete(passwordFile);
                     MessageBox.Show("Your Password has been saved successfully!!", "Password Saved", MessageBoxButtons.OK, MessageBoxIcon.Information);
                 }
                 
